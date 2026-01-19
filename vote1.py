02c86f062687ce8618d3ede8538d2ff1ce61f973
maresh990 <02fe24bca037@kletech.ac.in>
2026-01-19 12:51:57 +0530

    Create vote1.py

---
+++ vote1.py
@@ -0,0 +1,9 @@
+#vote.py
+try:
+age=int(input("Enter your age:"))
+if age >=18:
+print("your are eligible to vote")
+else
+print("your are not eligible to vote")
+except value error:
+print("please enter a valid integer for age")
